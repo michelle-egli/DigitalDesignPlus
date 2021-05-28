1553f43a659114257b774a74c6fa88d32f23dbb1
Michelle <michelle.egli.13@gmail.com>
2021-05-28 14:03:35 +0200

    test test

--- first_pages/main.js
+++ first_pages/main.js
@@ -3,11 +3,30 @@ gsap.defaults({ease: "none",duration:1});
 
 
 /*-----------------------------------------------animation screen 1: penis-------------------------------------------------------------*/
+const tl1 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".rect",
+        //markers: true,
+        start: "top 70%",
+        end: "top 35%",
+        scrub: 1,  
+    }
+})
 
+tl1.to(".rect", {x: -600, duration: 2});
+
+ScrollTrigger.create({
+    animation:tl1,
+    trigger: ".screen1",
+    start: "top top",
+    end:"+=1200",
+    scrub: 2,
+    pin: true,
+    anticipatePin: 1,
+});
 
 /*-----------------------------------------------animation screen 2: big fat fuck-------------------------------------------------------------*/
-/*funktioniert noch nicht!
-const tl = gsap.timeline({
+/*const tl2 = gsap.timeline({
     scrollTrigger: {
         trigger: ".screen2",
         markers: true,
@@ -17,29 +36,65 @@ const tl = gsap.timeline({
     }
 })
 
-tl.to(".fat_fuck", ".ha", ".big", ".opel", {x: -500, duration: 2});*/
+tl2.to(".titel", {x: 2000, duration: 2});
 
+
+/*
+tl2.to(".titel", {
+    transform: translateX(-2000),
+    ease: "none",
+    duration: 1,
+});*/
+
+/*
+ScrollTrigger.create({
+    animation:tl2,
+    trigger: ".screen2",
+    start: "top top",
+    end:"+=1200",
+    scrub: 2,
+    toggleClass: 'active',
+    pin: true,
+    anticipatePin: 1,
+});*/
+
+const tl2 = gsap.timeline();
+
+tl5.to(".titel",{
+    ease: "none",
+    duration: 1,
+ });
+ ScrollTrigger.create({
+   animation:tl2,
+   trigger: ".screen2",
+   start: "top" ,
+   end:"+=1200",
+   scrub: 2,
+   toggleClass: 'active',
+   markers:true,
+   pin: true,
+   anticipatePin: 1
+ });
 
 /*-----------------------------------------------animation screen 3: menstrutation-------------------------------------------------------------*/
 
 
 /*-----------------------------------------------animation screen 4: veganer-------------------------------------------------------------*/
-const tl = gsap.timeline();
-tl.to(".KastenRot2",{
+const tl4 = gsap.timeline();
+tl4.to(".KastenRot2",{
   y: -500,
     scaleY: 2.5,
     ease: "none",
     duration: 1,
 });
   ScrollTrigger.create({
-      animation:tl,
+      animation:tl4,
       trigger: ".screen4",
       start: "top top",
       end:"+=1200",
       scrub: 2,
       pin: true,
       anticipatePin: 1,
-    
   });
 
 // click Animation_________________________________________
@@ -61,15 +116,15 @@ function activate(i){
 }
 
 /*-----------------------------------------------animation screen 5: hoden-------------------------------------------------------------*/
-const tl2 = gsap.timeline();
+const tl5 = gsap.timeline();
 
-tl2.to(".Nation",{
+tl5.to(".Nation",{
     scale: 1,
    ease: "none",
     duration: 1,
  });
  ScrollTrigger.create({
-   animation:tl2,
+   animation:tl5,
    trigger: ".screen5",
    start: "top" ,
    end:"+=1200",
@@ -81,15 +136,15 @@ tl2.to(".Nation",{
  });
 
 /*-----------------------------------------------animation screen 6: mannequin-------------------------------------------------------------*/
-const tl3 = gsap.timeline();
+const tl6 = gsap.timeline();
 
-tl3.to(".screen6_2",{
+tl6.to(".screen6_2",{
     scale: 1,
     ease: "none",
     duration: 1,
 });
 ScrollTrigger.create({
-    animation:tl3,
+    animation:tl6,
     trigger: ".screen6",
     start: "top" ,
     end:"+=1200",
