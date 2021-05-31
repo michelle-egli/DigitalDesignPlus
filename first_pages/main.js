157033edb8b2af6b74ca9faaad8b37b770b52038
Michelle <michelle.egli.13@gmail.com>
2021-05-31 13:44:00 +0200

    merged code (animationen done)

--- first_pages/main.js
+++ first_pages/main.js
@@ -20,9 +20,83 @@ ScrollTrigger.create({
     start: "top top",
     end:"+=1200",
     scrub: 2,
-    pin: true,
-    anticipatePin: 1,
+    //pin: true,
+    //anticipatePin: 1,
 });
+
+const tl1_2 = gsap.timeline();
+
+tl1_2.to(".sperm",{
+     ease: "none",
+     duration: 3,
+     y: 500,
+     scale:1,
+    //  delay:1.2,
+  });
+  
+  ScrollTrigger.create({
+    animation:tl1_2,
+    trigger: ".screen1",
+    start: "top" ,
+    end:"+=1700",
+    scrub: 2,
+    toggleClass: 'active',
+    // markers: {
+    //   startColor:"green",
+    //   endColor: "orange",
+    // },
+    // pin: true,
+    //  anticipatePin: 1
+  
+  });
+  const tl1_3 = gsap.timeline();
+
+tl1_3.to(".sperm2",{
+     ease: "none",
+     duration: 1,
+     scale:2,
+     y: 500,
+    //  delay:1,
+  });
+  
+  ScrollTrigger.create({
+    animation:tl1_3,
+    trigger: ".screen1",
+    start: "top" ,
+    end:"+=1800",
+    scrub: 2,
+    toggleClass: 'active',
+    // markers: {
+    //   startColor:"green",
+    //   endColor: "orange",
+    // },
+      pin: true,
+      anticipatePin: 1
+  });
+
+  const tl1_4 = gsap.timeline();
+
+  tl1_4.to(".Flüssigkeit",{
+       ease: "none",
+       duration: 1,
+       y: -1000,
+    });
+    
+    ScrollTrigger.create({
+      animation:tl1_4,
+      trigger: ".screen1",
+      start: "top" ,
+      end:"+=2000",
+      scrub: 2,
+      toggleClass: 'active',
+      // markers: {
+      //   startColor:"green",
+      //   endColor: "orange",
+      // },
+      pin: true,
+       anticipatePin: 1
+    
+    });
 
 /*-----------------------------------------------animation screen 2: big fat fuck-------------------------------------------------------------*/
 const tl2 = gsap.timeline();
@@ -32,48 +106,112 @@ tl2.to(".titel",{
     duration: 1,
     x: 0,
  });
-
+ 
  ScrollTrigger.create({
    animation:tl2,
    trigger: ".screen2",
    start: "top" ,
-   end:"+=1200",
+   end:"+=2000",
    scrub: 2,
    toggleClass: 'active',
-   markers:true,
-   pin: true,
-   anticipatePin: 1
+  //  markers: {
+  //    startColor:"purple",
+  //    endColor: "yellow",
+  //  },
  });
+   
+ const tl2_1 = gsap.timeline();
 
-/*-----------------------------------------------animation screen 3: menstrutation-------------------------------------------------------------*/
-const tl3 = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".screen3",
-        markers: true,
-        start: "top 42%",
-        end: "top 35%",
-        scrub: 1,  
-    }
-})
-
-//tl3.to(".titel", {x: 1000, duration: 2});
-
-ScrollTrigger.create({
-    animation:tl3,
-    trigger: ".screen3",
+ tl2_1.to(".blacksquare",{
+     ease: "none",
+     duration: 1,
+     x: 0,
+  });
+  
+  ScrollTrigger.create({
+    animation:tl2_1,
+    trigger: ".screen2",
     start: "top" ,
-    end:"+=1200",
+    end:"+=6000",
     scrub: 2,
-    markers:true,
+    toggleClass: 'active',
+    // markers: {
+    //   startColor:"red",
+    //   endColor: "blue",
+    // },
     pin: true,
     anticipatePin: 1
   });
 
+
+/*-----------------------------------------------animation screen 3: menstrutation-------------------------------------------------------------*/
+const tl3_2 = gsap.timeline();
+
+tl3_2.to(".tear",{
+     ease: "none",
+     duration: 3,
+     y: 2000,
+     scale: 5,
+  });
+  
+  ScrollTrigger.create({
+    animation:tl3_2,
+    trigger: ".screen3",
+    start: "top" ,
+    end:"+=1000",
+    scrub: 2,
+    toggleClass: 'active',
+    // markers: {
+    //   startColor:"green",
+    //   endColor: "orange",
+    // },
+    pin: true,
+     anticipatePin: 1
+  
+  });
+ 
+  const tl3_3 = gsap.timeline();
+
+  tl3_3.to(".Mensblut",{
+      ease: "none",
+      duration: 1,
+      y: -1100,
+   });
+   
+   ScrollTrigger.create({
+     animation:tl3_3,
+     trigger: ".screen3",
+     start: "top" ,
+     end:"+=1000",
+     scrub: 2,
+     toggleClass: 'active',
+     //  pin: true,
+     //  anticipatePin: 1
+   });
+
 /*-----------------------------------------------animation screen 4: veganer-------------------------------------------------------------*/
+const tl4_2 = gsap.timeline();
+tl4_2.to(".KastenRot1",{
+  y: -1000,
+    scaleY: 2.5,
+    ease: "none",
+    duration: 1,
+});
+  ScrollTrigger.create({
+      animation:tl4_2,
+      trigger: ".screen4",
+      start: "top top",
+      end:"+=1200",
+      scrub: 2,
+      // pin: true,
+      // anticipatePin: 1,
+  });
+
+
 const tl4 = gsap.timeline();
 tl4.to(".KastenRot2",{
-  y: -500,
-    scaleY: 2.5,
+  y: 500,
+    // scaleY: 2.5,
     ease: "none",
     duration: 1,
 });
@@ -109,7 +247,7 @@ function activate(i){
 const tl5 = gsap.timeline();
 
 tl5.to(".Nation",{
-    scale: 1,
+    scale: 2,
    ease: "none",
     duration: 1,
  });
@@ -127,7 +265,6 @@ tl5.to(".Nation",{
 
 /*-----------------------------------------------animation screen 6: mannequin-------------------------------------------------------------*/
 const tl6 = gsap.timeline();
-
 tl6.to(".screen6_2",{
     scale: 1,
     ease: "none",
@@ -140,34 +277,77 @@ ScrollTrigger.create({
     end:"+=1200",
     scrub: 2,
     toggleClass: 'active',
-    markers:true,
-    pin: true,
-    anticipatePin: 1,
+    // markers:true,
+
+});
+
+const tl6_2= gsap.timeline();
+
+tl6_2.to(".screen6_2",{
+   ease: "none",
+   duration: 1,
+   y: -548,
+});
+ScrollTrigger.create({
+   animation:tl6_2,
+   trigger: ".screen6",
+   start: "top" ,
+   end:"+=5000",
+   scrub: 2,
+   toggleClass: 'active2',
+   markers: {
+   startColor:"pink",
+   endColor: "light blue",
+  },
+   pin: true,
+   anticipatePin: 1,
 });
 
 /*-----------------------------------------------animation screen 7: nippelsaft-------------------------------------------------------------*/
-const tl7 = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".screen7",
-        markers: true,
-        start: "top 42%",
-        end: "top 35%",
-        scrub: 1,  
-    }
-})
+const tl7 = gsap.timeline();
 
-//tl3.to(".titel", {x: 1000, duration: 2});
+tl7.to(".Milch",{
+    scale: 2,
+   ease: "none",
+    duration: 1,
+ });
+ ScrollTrigger.create({
+   animation:tl7,
+   trigger: ".screen7",
+   start: "top" ,
+   end:"+=7000",
+   scrub: 2,
+   toggleClass: 'active',
+  //  markers: {
+  //   startColor:"pink",
+  //   endColor: "violett",
+  // },
+   pin: true,
+   anticipatePin: 1
+ });
 
-ScrollTrigger.create({
-    animation:tl7,
-    trigger: ".screen7",
-    start: "top" ,
-    end:"+=1200",
-    scrub: 2,
-    markers:true,
-    pin: true,
-    anticipatePin: 1
-  });
+ const tl7_2 = gsap.timeline();
+
+tl7_2.to(".Milch2",{
+    scale: 1.5,
+    translateX:-300,
+   ease: "none",
+    duration: 1,
+ });
+ ScrollTrigger.create({
+   animation:tl7_2,
+   trigger: ".screen7",
+   start: "top" ,
+   end:"+=7000",
+   scrub: 2,
+   toggleClass: 'active',
+  //  markers: {
+  //   startColor:"pink",
+  //   endColor: "violett",
+  // },
+   pin: true,
+   anticipatePin: 1
+ });
 
 /*-----------------------------------------------animation screen 8: flachbrüstigkeit-------------------------------------------------------------*/
 const tl8 = gsap.timeline({})
